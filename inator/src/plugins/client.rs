629da47b431fe8b9f09ba3923bd3fbfe30832c7c
Kevenpvp <76878791+Kevenpvp@users.noreply.github.com>
2026-01-19 07:50:19 -0300

    Small Changes

--- inator/src/plugins/client.rs
+++ inator/src/plugins/client.rs
@@ -18,9 +18,6 @@ pub(crate) struct ClientConnectionPlugin;
 impl Plugin for ClientConnectionPlugin{
     fn build(&self, app: &mut App) {
         app.add_systems(First, (check_main_port_dropped,start_connections,check_connected_to_main_port,check_listening_to_main_port).chain());
-
-        #[cfg(target_arch = "wasm32")]
-        app.add_systems(First,check_messages_queued_main_port.after(check_listening_to_main_port));
     }
 }
 
@@ -58,39 +55,6 @@ fn check_main_port_dropped(
                 continue
             }
         }
-
-        /*
-        match main_port {
-            #[cfg(target_arch = "wasm32")]
-            PortClient::Wasm(client_wasm) => {
-                match client_wasm.connection_down_receiver.try_recv() {
-                    Ok(_) => {
-                        client_wasm.connected = false;
-                        client_wasm.connecting = false;
-                        client_wasm.listening = false;
-                        client_connection.connected = false;
-
-                        match client_wasm.split_sink.take() {
-                            None => {}
-                            Some(split_sink) => { drop(split_sink) }
-                        };
-
-                        match client_wasm.split_stream.take() {
-                            None => {}
-                            Some(split_stream) => { drop(split_stream) }
-                        };
-
-                        if client_wasm.settings.try_reconnect { continue }
-
-                        disconnect_vector.push(client_connection.connection_name);
-                    }
-                    Err(_) => {
-                        continue;
-                    }
-                }
-            }
-            _ => {}
-        } */
     }
 
     for connection_name in disconnect_vector {
@@ -112,27 +76,6 @@ fn check_connected_to_main_port(
                 continue
             }
         }
-
-        /*
-        match main_port {
-            #[cfg(target_arch = "wasm32")]
-            PortClient::Wasm(client_wasm) => {
-                if client_wasm.connected { continue; }
-
-                match client_wasm.connected_to_server_receiver.try_recv() {
-                    Ok(web_socket) => {
-                        client_wasm.connected = true;
-
-                        let (sink,stream) = web_socket.split();
-
-                        client_wasm.split_sink = Some(Rc::new(RefCell::new(sink)));
-                        client_wasm.split_stream = Some(Rc::new(RefCell::new(stream)));
-                    }
-                    Err(_) => {}
-                }
-            }
-            _ => {}
-        }*/
     }
 }
 
